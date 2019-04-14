f71f00f64632c58477a018ba3396cae585f89a7b
Nick Tsoukas <nick.tsoukas101@gmail.com>
2019-04-14 16:56:08 -0500

    Refactored authentication service >> uses firebase auth methods >> next create user roles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 
 @Component({
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client-management';
+
+  constructor(private authService: AuthService){
+
+  }
+  ngOnInit(){
+    this.authService.authListener();
+  }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,48 +7,52 @@ import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
     providedIn: 'root',
-  })
+})
 export class AuthService {
-    authChange =  new Subject<boolean>();
-    private isAuthenticated : boolean = false ;
-    
-    constructor(private router: Router, private afAuth: AngularFireAuth){
+    authChange = new Subject<boolean>();
+    private isAuthenticated: boolean = false;
 
+    constructor(private router: Router, private afAuth: AngularFireAuth) { }
+
+    authListener() {
+        this.afAuth.authState.subscribe(user => {
+            if (user) {
+                this.isAuthenticated = true;
+                this.authChange.next(true);
+                this.router.navigate(['./listings']);
+            } else {
+                this.isAuthenticated = false;
+                this.authChange.next(false);
+                this.router.navigate(["./login"]);
+            }
+        })
     }
 
-    registerUser(authData: AuthData){
+    registerUser(authData: AuthData) {
         this.afAuth.auth.createUserWithEmailAndPassword(authData.email, authData.password)
             .then(res => {
-                this.authSuccessfully();
+                console.log(res)
             })
             .catch(err => {
                 console.log(err)
             })
     }
 
-    logIn(authData: AuthData){
-       this.afAuth.auth.signInWithEmailAndPassword(authData.email, authData.password)
-        .then(res => {
-            this.authSuccessfully();
-        })
-        .catch(err => {
-            console.log(err)
-        })
+    logIn(authData: AuthData) {
+        this.afAuth.auth.signInWithEmailAndPassword(authData.email, authData.password)
+            .then(res => {
+                return res
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }
 
-    logOut(){
-        this.isAuthenticated = false;
-        this.authChange.next(false);
-        this.router.navigate(['./login']);
+    logOut() {
+        this.afAuth.auth.signOut();
     }
 
-    isAuth(){
-       return this.isAuthenticated
-    }
-
-    private authSuccessfully(){
-        this.isAuthenticated = true;
-        this.authChange.next(true);
-        this.router.navigate(['./listings']);
+    isAuth() {
+        return this.isAuthenticated
     }
 }
