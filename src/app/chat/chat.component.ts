de8cea66bd191d97a0a2a90231c6e00fbbfe13ba
Nick Tsoukas <nick.tsoukas101@gmail.com>
2019-04-30 20:38:08 -0500

    Done with auto scroll and admin chat ui on mobile ... still lots to do ..

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,38 +1,48 @@
-import { AfterViewInit, Component, OnInit, ViewChild, Input} from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, Input } from '@angular/core';
 import { Observable } from 'rxjs'
-import { ActivatedRoute} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
-import { ChatServiceService} from './chat-service.service';
+import { ChatServiceService } from './chat-service.service';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { map } from 'rxjs/operators';
 
-interface Chat {
-  uid: string;
-  message: string;
-}
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
-   userId: string;
-   @Input()
-   chats: Observable<any>;
+  userId: string;
+  @Input()
+  chats: Observable<any>;
+  ids: any;
+  ar: string[];
 
-   onSubmit(formData){
-    this.chatService.addMessage(this.userId,formData.value.message);
-   }
+  deleteAll(){
+    this.ar.forEach((val) => {
+      this.chatService.delete(this.userId,val);
+    })
+    }
+
+  onSubmit(formData) {
+    this.chatService.addMessage(this.userId, formData.value.message);
+    formData.reset();
+  }
 
 
-  constructor(private chatService: ChatServiceService, private route: ActivatedRoute, private db : AngularFirestore) { 
-    
+  constructor(private chatService: ChatServiceService, private route: ActivatedRoute, private db: AngularFirestore) {
+
     this.userId = this.route.snapshot.data['chat']['uid'];
 
   }
   ngOnInit() {
     this.chats = this.chatService.getMes(this.userId);
+    this.ids = this.chatService.getAllId(this.userId);
+    // subscribe to set val of ar to an Array of ids 
+    this.ids.subscribe(val => {
+      this.ar = val;
+    })
   }
 
 }
